0c0a02344fd1195e7eb29ad6ffce07aa8d3caae6
chrisk <christian.khalil200@gmail.com>
2021-05-03 09:06:01 +0200

    Add project files.

---
+++ BusyList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
