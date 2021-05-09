81ba76218090f28c97a44bd112c87c51d66a2398
unbekanntunity <nguyen3lihn@gmail.com>
2021-05-09 19:17:44 +0200

    Changed some prints

--- BusyList/Handlers/ListHandler.cs
+++ BusyList/Handlers/ListHandler.cs
@@ -19,6 +19,8 @@ namespace BusyList.Handlers
 
             string seperator = new String('-', 10);
 
+            Console.WriteLine("Tasks:\n");
+
             foreach (var item in tasks)
             {
                 Console.WriteLine(seperator);
