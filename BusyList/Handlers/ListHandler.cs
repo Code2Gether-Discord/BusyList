1df06a14058afeb1bac949b8087cd3e362a41f80
unbekanntunity <nguyen3lihn@gmail.com>
2021-05-09 19:15:31 +0200

    Add list command

---
+++ BusyList/Handlers/ListHandler.cs
@@ -0,0 +1,32 @@
+﻿using BusyList.Commands;
+using System;
+using System.Collections.Generic;
+
+namespace BusyList.Handlers
+{
+    public class ListHandler : IHandler<DoneCommand>
+    {
+        private readonly ITaskRepository _taskRepository;
+
+        public ListHandler(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        public void Run(DoneCommand command)
+        {
+            IEnumerable<TaskItem> tasks = _taskRepository.GetAll();
+
+            string seperator = new String('-', 10);
+
+            foreach (var item in tasks)
+            {
+                Console.WriteLine(seperator);
+                Console.WriteLine($"Task id: {item.Id}");
+                Console.WriteLine($"Description: {item.Description}");
+                Console.WriteLine($"Status: {item.TaskStatus}");
+                Console.WriteLine(seperator);
+            }
+        }
+    }
+}
