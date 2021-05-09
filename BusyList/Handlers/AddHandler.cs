cd3195eecdfaddb7d2f21af13572c9e9aa48b543
denislavh <53407843+denislavh@users.noreply.github.com>
2021-05-09 19:29:13 +0300

    Implemented Add command

---
+++ BusyList/Handlers/AddHandler.cs
@@ -0,0 +1,26 @@
+﻿using BusyList.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusyList.Handlers
+{
+    public class AddHandler : IHandler<AddCommand>
+    {
+        private ITaskRepository _taskRepository;
+
+        public AddHandler(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+        public void Run(AddCommand command)
+        {
+            AddTaskData item = new AddTaskData(command.Description);
+            _taskRepository.AddTask(item);
+
+            Console.WriteLine($"Added task with description:{item.Description}");
+        }
+    }
+}
