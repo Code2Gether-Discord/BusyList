4c1aac2272ca5721158e433962ff7174051eac3a
Milo <mlk_s@DESKTOP-LB2PMR1.(none)>
2021-05-12 14:05:35 +0200

    Added Tests for ReadHandler

---
+++ tests/BusyList.Tests/Handlers/ReadHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using BusyList.Commands;
+using BusyList.Handlers;
+using Moq;
+using Xunit;
+
+namespace BusyList.Tests.Handlers
+{
+    public class ReadHandlerTests
+    {
+        private readonly Mock<ITaskRepository> _mockRepository = new();
+        private readonly ReadHandler _handler;
+
+        public ReadHandlerTests()
+        {
+            _handler = new ReadHandler(_mockRepository.Object);
+        }
+
+        [Fact]
+        public void Run_ShouldSetTaskStatusToDone_WhenAValidIdIsPassed()
+        {
+            var command = new ReadCommand(1);
+            var task = new TaskItem(command.Id, "Test Description");
+
+            _mockRepository.Setup(_ => _.GetTaskById(command.Id)).Returns(task);
+
+            _handler.Run(command);
+
+            _mockRepository.Verify(_ => _.GetTaskById(command.Id));
+        }
+    }
+}
